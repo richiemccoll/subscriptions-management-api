1e68a7867ca0b0022d78a8b91281635cb4459f2c
Richie McColl <richie.mccoll@hotmail.co.uk>
2019-05-31 13:23:47 +0100

    Add plans service.

---
+++ services/plans-service.js
@@ -0,0 +1,17 @@
+export default class PlansService {
+    async findAll(){
+        return [];
+    }
+
+    async findOne(id) {
+
+    }
+
+    async create(plan) {
+
+    }
+
+    async deleteOne(id) {
+
+    }
+}
