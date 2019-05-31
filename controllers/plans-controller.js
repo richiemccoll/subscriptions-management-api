51239ddaf0f5008177d04082a8edd5304f832936
Richie McColl <richie.mccoll@hotmail.co.uk>
2019-05-31 13:09:25 +0100

    Add plans controller skeleton.

---
+++ controllers/plans-controller.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+const router = new Router();
+
+// GET api/plans/
+router.get('/', (req, res) => {
+    
+})
+
+// GET api/plans/:id
+router.get('/:id', (req, res) => {
+    
+})
+
+// POST api/plans/
+router.post('/', (req, res) => {
+    
+})
+
+// DELETE api/plans/
+router.delete('/:id', (req, res) => {
+    
+})
+
+export default router;
