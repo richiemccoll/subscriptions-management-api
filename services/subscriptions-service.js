0368ab8028dccd3912bfcde97202ebb33644a9bc
Richie McColl <richie.mccoll@hotmail.co.uk>
2019-05-31 13:24:33 +0100

    Add subscriptions service.

---
+++ services/subscriptions-service.js
@@ -0,0 +1,17 @@
+export default class SubscriptionsService {
+    async findAll(userId) {
+
+    }
+
+    async findOne(id) {
+
+    }
+
+    async create(subscription) {
+
+    }
+
+    async deleteOne(id) {
+        
+    }
+}
