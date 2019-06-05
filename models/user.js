9e0c06fea0bd702b802f8dc77f2de5665c3aac16
Richie McColl <richie.mccoll@hotmail.co.uk>
2019-06-05 10:37:19 +0100

    Add user model, schema and migration.

---
+++ models/user.js
@@ -0,0 +1,32 @@
+import Joi from "joi";
+
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+    },
+    {}
+  );
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
+
+const UserValidationSchema = Joi.object().keys({
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string()
+    .email()
+    .required(),
+  password: Joi.string()
+    .required()
+    .min(6)
+    .max(32)
+});
+
+export { UserValidationSchema, UserModel as default };
